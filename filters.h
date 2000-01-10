31d0174c42a8ac511d507307c3d2c859aa3e3ad7
nobody <nobody@localhost>
2000-01-10 00:45:03 +0000

    This commit was manufactured by cvs2svn to create branch 'debug-mat'.
    svn path=/branches/debug-mat/lame/; revision=115

--- filters.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILTERS_H_INCLUDED
-#define FILTERS_H_INCLUDED
-
-#include "util.h"
-void filterMDCT( FLOAT8 xr_org[2][2][576], 
-		III_side_info_t *l3_side, frame_params *fr_ps);
-
-
-#endif
