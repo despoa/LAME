aef330c908b40fd66cace07bbab1152770f65a57
nobody <nobody@localhost>
2000-07-31 19:00:57 +0000

    This commit was manufactured by cvs2svn to create branch 'debug'.
    svn path=/branches/debug/lame/; revision=922

--- reservoir.h
+++
@@ -1,9 +0,0 @@
-#ifndef RESERVOIR_H
-#define RESERVOIR_H
-
-int ResvFrameBegin( lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits, int frameLength );
-void ResvMaxBits(lame_global_flags *gfp, int mean_bits, int *targ_bits, int *max_bits);
-void ResvAdjust(lame_global_flags *gfp,gr_info *gi, III_side_info_t *l3_side, int mean_bits );
-void ResvFrameEnd(lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits );
-
-#endif
